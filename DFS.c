af8972100e748d9473f21d7be4f3183ecb355d11
yasaswi2112 <yasaswiinampudi@gmail.com>
2026-03-05 19:17:50 +0530

    Create DFS.c

---
+++ DFS.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#define MAX 10
+
+int graph[MAX][MAX];
+int visited[MAX];
+int n;
+
+void dfs(int v) {
+    visited[v] = 1;
+    printf("%d ", v);
+
+    for (int i = 0; i < n; i++) {
+        if (graph[v][i] == 1 && visited[i] == 0) {
+            dfs(i);
+        }
+    }
+}
+
+int main() {
+    int start;
+
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    printf("Enter adjacency matrix:\n");
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            scanf("%d", &graph[i][j]);
+
+    printf("Enter starting vertex: ");
+    scanf("%d", &start);
+
+    printf("DFS Traversal: ");
+    dfs(start);
+
+    return 0;
+}
